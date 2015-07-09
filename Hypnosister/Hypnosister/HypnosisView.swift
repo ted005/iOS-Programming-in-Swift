6c1785befb85b00d82d36e0d488b83187e220e57
ted005 <witwiky@me.com>
2015-07-09 23:19:59 +0800

    demo code for chapter 4.3

---
+++ Hypnosister/Hypnosister/HypnosisView.swift
@@ -0,0 +1,14 @@
+//
+//  HypnosisView.swift
+//  Hypnosister
+//
+//  Created by Robbie on 15/7/9.
+//  Copyright (c) 2015年 Ted Wei. All rights reserved.
+//
+
+import UIKit
+
+class HypnosisView :  UIView{
+ 
+    
+}
